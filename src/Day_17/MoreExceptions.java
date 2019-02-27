f17eb087903bd55f58625dcd2507e68202de04b3
mariesto <sitinjakamendo@gmail.com>
2019-02-27 14:18:42 +0700

    Day 17

---
+++ src/Day_17/MoreExceptions.java
@@ -0,0 +1,39 @@
+package Day_17;
+
+import java.util.Scanner;
+
+class Calculator{
+
+     int power(int n, int p) throws Exception {
+        int rs = 0;
+
+        if (n < 0 || p < 0){
+            throw new Exception("n and p should be non-negative");
+        }else {
+            rs = (int) Math.pow(n, p);
+        }
+
+        return rs;
+     }
+}
+
+public class MoreExceptions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
